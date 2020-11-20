16f47b58cee7cc0c8eeba8c59919a9c01a90e214
suren <elek34ka@gmail.com>
2020-11-20 19:59:56 +0300

    pep8

--- test.py
+++ test.py
@@ -6,7 +6,8 @@ events_dir = os.listdir(path="event")
 schemas_dir = os.listdir(path="schema")
 
 with open('readme.md', 'w', encoding='utf8') as readme:
-    readme.write('test.py is the main script file. It will work fine after jsonschema module installation.\n\nResults:\n\n')
+    readme.write(
+        'test.py is the main script file. It will work fine after jsonschema module installation.\n\nResults:\n\n')
 
 for schema in schemas_dir:
     with open('schema/{}'.format(schema)) as schema_file:
@@ -20,4 +21,5 @@ for schema in schemas_dir:
 
         for error in sorted(v.iter_errors(result), key=str):
             with open('readme.md', 'a', encoding='utf8') as readme:
-                readme.write('Schema: {}\nEvent: {}\nError: {}\n\n'.format(schema, event, error.message))
+                readme.write('Schema: {}\nEvent: {}\nError: {}\n\n'.format(
+                    schema, event, error.message))
